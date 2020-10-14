144706e4dcedb40a0385e213e41a7c5426856b04
beltzawoo <24609727+beltzawoo@users.noreply.github.com>
2020-10-14 16:36:42 +0200

    'Making the first 'usable' version'

--- disowned.py
+++ disowned.py
@@ -5,9 +5,20 @@ from math import ceil
 from shutil import copyfile
 import os
 import subprocess
-import ffmpeg
+import glob
 
-#TODO: Implement ffmpeg extracting
+# Clean previous files
+frames = glob.glob("tmp/frames/*")
+for f in frames:
+    os.remove(f)
+pages = glob.glob("tmp/pages/*")
+for f in pages:
+    os.remove(f)
+
+# Extract the frames with ffmpeg @ 24fps
+input_file = os.listdir("input/")
+cmd = "ffmpeg -i input/{} -r 24 -vf scale=1335:-1 tmp/frames/f%d.png".format(input_file[0])
+subprocess.run(cmd, shell=True)
 
 frame_list = os.listdir("tmp/frames/")
 blank_frames = 4 - len(frame_list)%4
@@ -25,7 +36,7 @@ number_of_pages = int(len(frame_list)/4)
 # Make the inital .jpg pages
 for page in range(0, number_of_pages):
     print("Page number " + str(page))
-    cmd = "montage -gravity East -geometry '1185x750+150+150' tmp/frames/{} tmp/frames/{} tmp/frames/{} tmp/frames/{} tmp/pages/{}.jpg".format(
+    cmd = "montage -gravity East -geometry '1485x1050>+0+0' tmp/frames/{} tmp/frames/{} tmp/frames/{} tmp/frames/{} tmp/pages/{}.png".format(
         frame_list[0], frame_list[1], frame_list[2], frame_list[3], page
     )
     subprocess.run(cmd, shell=True)
@@ -33,7 +44,7 @@ for page in range(0, number_of_pages):
 
 # Overlay the borders to the pages
 for page in range(0, number_of_pages):
-    cmd = "composite -gravity center resources/overlay.png tmp/pages/{page}.jpg tmp/pages/{page}.jpg".format(
+    cmd = "composite -gravity center resources/dumboverlay.png tmp/pages/{page}.png tmp/pages/{page}.png".format(
         page = page
     )
     subprocess.run(cmd, shell=True)
@@ -43,5 +54,5 @@ pdf = FPDF()
 pdf.set_auto_page_break(0)
 for page in range(0, number_of_pages):
     pdf.add_page(orientation = "L")
-    pdf.image("tmp/pages/{}.jpg".format(page), 0, 0, 297, 210)
+    pdf.image("tmp/pages/{}.png".format(page), 0, 0, 297, 210)
 pdf.output("./output/flipbook.pdf", "F")
